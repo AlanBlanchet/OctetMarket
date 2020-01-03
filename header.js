82e8e02fab984004232f70e463f1a3443501d337
XxAlanoxxX <alanblanchet@orange.fr>
2020-01-03 18:10:42 +0100

    presque fini header et footer

--- header.js
+++ header.js
@@ -55,8 +55,5 @@ function SetBurger()
 	document.getElementsByClassName("nav-menu")[0].style.left=(burger)?"-260px":"0px";
 	document.getElementsByClassName("nav-menu")[0].style.maxWidth=(burger)?"200px":"100vw";
 	document.getElementsByClassName("burger")[0].style.backgroundColor=(burger)?"var(--main-color)":"#003e7e";
-	//document.getElementsByTagName("nav")[0].setAttribute('style',(burger)?'background-color:var(--overlay-dark);':'background-color:#003e7e;');
-	//document.getElementsByTagName("nav")[0].style.backgroundColor=(burger)?"var(--main-color)":"#003e7e";
-
 	burger = !burger;
 }
